2ca3cc09651b01f1a2aba4f4184fa978bdb9ecf4
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2019-03-30 01:28:28 +0000

    Fix warnings in main program

--- src/main/java/com/wynprice/curseforgemaven/Gui.java
+++ src/main/java/com/wynprice/curseforgemaven/Gui.java
@@ -9,7 +9,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
--- src/main/java/com/wynprice/curseforgemaven/OptionalDownloadGui.java
+++ src/main/java/com/wynprice/curseforgemaven/OptionalDownloadGui.java
@@ -9,7 +9,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
